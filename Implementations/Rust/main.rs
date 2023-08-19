cfe00ddf83161c84f004fdcdb818faab467df542
errubec <83511647+errubec@users.noreply.github.com>
2023-08-19 02:20:44 +0100

    Tweak Rust code

--- Implementations/Rust/main.rs
+++ Implementations/Rust/main.rs
@@ -9,8 +9,9 @@ fn main() {
 
     let seed: String = (timestamp / 32 - 1).to_string() // does not end in 0
         .chars().rev().collect(); // does not start with 0
-    let seed: u64 = seed.parse()
-        .expect(&format!("Could not parse '{}' into u64", seed));
+    let seed: u64 = seed.parse().expect(
+        &format!("Could not parse '{seed}' into u64")
+    );
 
     println!("{:#}", radix_36(seed)); // Generated ID
 }
